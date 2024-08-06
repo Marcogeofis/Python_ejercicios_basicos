9270816dfd7e7b2283e5f761585cee447ddf4b73
Marco Antonio <marcogeofis@gmail.com>
2024-08-06 12:38:48 -0600

    Guarde algunos programas en una carpeta que contiene todo sobre conjuntos

--- Practica/modulo_pypdf2/read_pdf.py
+++ Practica/modulo_pypdf2/read_pdf.py
@@ -1,5 +1,5 @@
 import PyPDF2
 
-reader = PyPDF2.PdfReader('4.pdf')
-page = reader.pages[0]
-print(page.extract_text(0, 99))
+pdf_file = open('/home/jahrebel/Documentos/Lenguejes_Programacion/Python/Libros_sobre_Pytho/pandas.pdf', 'rb')
+pdf_reader = PyPDF2.PdfFileReader(pdf_file)
+print(pdf_reader.documentInfo)
